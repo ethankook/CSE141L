61c5b9eaf5de8bb7781b1bd890bd1e7c199272b4
Ethan Kook <ethankook1@gmail.com>
2024-12-01 17:36:06 -0800

    m

--- assembler.cpp
+++ assembler.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 
 // converts register to binary
-string registerToBinary(const string &reg){ 
+string registerToBinary3bits(const string &reg){ 
     if(reg == "R0" || reg == "r0") return "000";
     if(reg == "R1" || reg == "r1") return "001";
     if(reg == "R2" || reg == "r2") return "010";
@@ -20,6 +20,16 @@ string registerToBinary(const string &reg){
     if(reg == "R5" || reg == "r5") return "101";
     if(reg == "R6" || reg == "r6") return "110";
     if(reg == "R7" || reg == "r7") return "111";
+    return "";
+
+}
+
+string registerToBinary2bits(const string &reg){ 
+    if(reg == "R0" || reg == "r0") return "00";
+    if(reg == "R1" || reg == "r1") return "01";
+    if(reg == "R2" || reg == "r2") return "10";
+    if(reg == "R3" || reg == "r3") return "11";
+
     return "";
 
 }
@@ -32,15 +42,21 @@ string immediateToBinary(const string &immediate, int n){
 // encodes most R type instructions
 string encodeR(const vector<string> &tokens, const unordered_map<string, string> &opcodes){
     string opcode = opcodes.at(tokens[0]);
-    string registerFileAddress1 = registerToBinary(tokens[1]);
-    string registerFileAddress2 = registerToBinary(tokens[2]); 
+    string registerFileAddress1 = registerToBinary2bits(tokens[1]);
+    string registerFileAddress2 = registerToBinary3bits(tokens[2]); 
     return "0" + registerFileAddress1 + registerFileAddress2 + opcode;
 }
 
+string encodeLdr(const vector<string> &tokens, const unordered_map<string, string> &opcodes){
+    string opcode = opcodes.at(tokens[0]);
+    string registerFileAddress1 = registerToBinary2bits(tokens[1]);
+    string registerFileAddress2 = registerToBinary2bits(tokens[2]); 
+    return "0" + registerFileAddress2 + registerFileAddress1 + opcode;
+}
 //encodes B type instructions
 string encodeB(const vector<string> &tokens, const unordered_map<string, string> &opcodes) {
     string opcode = opcodes.at(tokens[0]);
-    string registerfileAddress = registerToBinary(tokens[1]);
+    string registerfileAddress = registerToBinary2bits(tokens[1]);
     string lutIndex = immediateToBinary(tokens[2], 5);
     return "1" + registerfileAddress + opcode + lutIndex;
 }
@@ -59,14 +75,14 @@ string encodeMov(const vector<string> &tokens, const unordered_map<string, strin
 //encodes Mot/ lso/ Mbs
 string encodeMot(const vector<string> &tokens, const unordered_map<string, string> &opcodes){
     string opcode = opcodes.at(tokens[0]);
-    string registerFileAddress = registerToBinary(tokens[1]);
+    string registerFileAddress = registerToBinary3bits(tokens[1]);
     string direction = immediateToBinary(tokens[2], 1);
     return "0" + registerFileAddress + direction + opcode;
 }
 //encodes cfb instruction
 string encodeCfb(const vector<string> &tokens, const unordered_map<string, string> &opcodes){
     string opcode = opcodes.at(tokens[0]);
-    string registerFileAddress = registerToBinary(tokens[1]);
+    string registerFileAddress = registerToBinary3bits(tokens[1]);
     return "0" + registerFileAddress + "0" + opcode;
 }
 
@@ -157,6 +173,12 @@ void assemble (const string &inputFile, const string &outputFile){
                     continue;
                 }
                 instruction = encodeB(tokens, opcodes);
+            } else if (tokens[0]=="ldr") {
+                if (tokens.size() != 3) {
+                    cerr << "Error: invalid number of arguments at line: " << line << endl;
+                    continue;
+                }
+                instruction = encodeLdr(tokens, opcodes);
             } else {
                 if (tokens.size() != 3) {
                     cerr << "Error: invalid number of arguments at line: " << line << endl;
